2152a65b520d1451b4ed32650dd1cb7f94ab4300
thomasrichardson439 <richardsonthomas439@gmail.com>
2019-11-07 20:01:47 +0200

    updated version

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
+    <div class="w-full p-8">
         <h1>Home</h1>
         <h5>Test: {{test}}</h5>
+        <br>
+        <ag-button title="submit" type="full" :disabled="true">
+        </ag-button>
+        <br>
+        <ag-button  type="full" >
+
+        </ag-button>
     </div>
 </template>
 
---
+++ resources/js/components/form/AgButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-auto  w-max-220 rounded-full flex justify-center items-center text-center  border-ag-button "
+         @click="$emit('click')"
+         @mouseover="$emit('mouseover')"
+         @mouseleave="$emit('mouseleave')"
+         :class="[disabled ? 'cursor-not-allowed ' : 'cursor-pointer' , type == 'full' ? 'bg-ag-button text-white' : 'bg-white text-ag-button' , title ? '' : 'w-min-30']">
+        <p v-if="title" class="uppercase px-6 py-2">{{title}}</p>
+        <slot name="icon"></slot>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            title: String,
+            type: String,
+            disabled:{
+                type : Boolean,
+                default : false
+            }
+        }
+    }
+</script>
+<style>
+    .bg-ag-button{
+        background: #525CA3;
+    }
+    .text-ag-button {
+        color: #525CA3;
+    }
+    .border-ag-button {
+        border: 2px solid #525CA3;
+    }
+    .w-max-220 {
+        max-width: 220px;
+    }
+    .w-min-30 {
+        max-width : 42px;
+        max-height: 42px;
+        width : 42px;
+        height: 42px
+    }
+</style>
