a257578cbcf39e8f5d9608eb38d570257631f347
thomasrichardson439 <richardsonthomas439@gmail.com>
2019-11-12 17:34:27 +0200

    subheader-with-popups (padding, blocks padding)

---
+++ resources/js/components/Layout/AgSubHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="w-full rounded-full bg-gray-300 flex justify-between text-center relative">
+    <div class="w-1/2 flex justify-start">
+        <div class="w-2/4 border-r border-gray-500 relative pt-3 p-3 text-left">
+            <input type="text" placeholder="search..." class="pl-8 bg-transparent rounded-full placeholder-brand-500 text-brand-500 pt-pxx" v-model="searchValue" @keyup="search()">
+            <img src="/assets/icons/search.svg" alt="" class="absolute left-0 top-0 ml-4 mt-4">
+        </div>
+        <div class="w-1/4 border-r border-gray-500 p-3 cursor-pointer flex justify-center relative" @click="openFilter()">
+            <p class=" text-brand-500">FILTER </p> <img src="/assets/icons/arrow-down.svg" alt="" class=" pl-2 py-1 ">
+
+        </div>
+        <div class="w-1/4 border-r border-gray-500 p-3 cursor-pointer flex justify-center relative" >
+            <p @click="openBlocks()" class=" text-brand-500">BLOCKS </p> <img @click="openBlocks()" src="/assets/icons/arrow-down.svg" alt="" class=" pl-2 py-1">
+            <ag-blocks-box v-if="showBlocks"></ag-blocks-box>
+        </div>
+    </div>
+    <div class="w-1/2 flex justify-start">
+        <div class="w-1/3 flex justify-start border-r border-gray-500">
+        </div>
+        <div class="w-2/3 flex justify-start">
+            <div class="w-2/5 border-r border-gray-500 p-3 cursor-pointer flex justify-center">
+                <p class=" text-brand-500">EXPORT XLS </p> <img src="/assets/icons/download.svg" alt="" class=" pl-2 py-1 ">
+            </div>
+            <div class="w-2/5 border-r border-gray-500 p-3 cursor-pointer flex justify-center">
+                <p class=" text-brand-500">EXPORT CSV </p> <img src="/assets/icons/download.svg" alt="" class=" pl-2 py-1 ">
+            </div>
+            <div class="w-1/5 p-3 flex justify-center cursor-pointer" @click="remove()"> <img src="/assets/icons/delete.svg" alt=""  > </div>
+        </div>
+    </div>
+        <ag-filter-box v-if="showFilter"></ag-filter-box>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ag-sub-header',
+        data(){
+            return {
+                searchValue: '',
+                showBlocks: false,
+                showFilter: false
+            }
+        },
+        methods: {
+            search(){
+
+            },
+            remove(){
+
+            },
+            openFilter(){
+                this.showBlocks = false
+                this.showFilter = !this.showFilter
+            },
+            openBlocks(){
+                this.showFilter = false
+                this.showBlocks = !this.showBlocks
+            }
+        }
+    }
+
+</script>
+<style>
+
+</style>
---
+++ resources/js/components/boxes/AgFilterBox.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class=" w-full flex absolute bg-white top-0 mt-16 mx-auto shadow-popup rounded z-20">
+        <div class=" absolute top-0 -mt-5 w-full flex justify-center z-30 white-tringle">
+        </div>
+        <div class="w-full flex flex-col pt-5 p-3 h-full">
+            <div class="w-full flex justify-start" >
+                <div class="flex mx-2 py-px cursor-pointer"
+                     @click="chooseTab(tab.id , tab.slug)"
+                     :class="selectedTab === tab.id ? 'border-b-2 border-brand-600' : ''"
+                     v-for="(tab , index) in filterTabs"
+                     :key="index"
+                >
+                    <p class=" uppercase" :class="selectedTab === tab.id ? 'text-brand-500' : 'text-gray-500'">{{tab.name}}</p>
+                </div>
+            </div>
+            <div class="w-full flex flex-col">
+                <div class="w-full flex justify-start flex-wrap pb-5 p-2" v-for="tab in filterTabs" :key="tab.id" v-if="selectedTab === tab.id">
+                    <div class="w-1/3 text-left p-2" v-for="input in tab.inputs">
+                        <ag-text-input  label="Brand"></ag-text-input>
+                    </div>
+                </div>
+                <div class="w-full flex justify-end px-4 pb-2">
+                    <ag-button type="full" class="px-8 mx-2" title="reset" @click="resetFilter()"></ag-button>
+                    <ag-button class="mx-2 px-8" type="full" title="apply" @click="applyFilter()"></ag-button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ag-filter-box',
+        data(){
+            return {
+                filterTabs: [
+                    {id: 0 , name: 'Campaign' , slug: 'campaign' , inputs: 12},
+                    {id: 1 , name: 'Line Item' , slug: 'line_item' , inputs : 7},
+                    {id: 2 , name: 'Inseration Order' , slug: 'insertion_order' , inputs: 5},
+                    {id: 3 , name: 'Creative' , slug: 'creative' , inputs: 2},
+                ],
+                selectedTab : 0,
+            }
+        },
+        methods: {
+            chooseTab(id , slug){
+                this.selectedTab = id
+            },
+            resetFilter(){
+
+            },
+            applyFilter(){
+
+            }
+        }
+
+    }
+</script>
+<style>
+    .white-tringle {
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0 10px 20px 10px;
+        border-color: transparent transparent #fff transparent;
+        right:68%;
+        -webkit-filter: drop-shadow(1px -1px 2px  rgba(0,0,0,.13));
+        filter: drop-shadow(1px -1px 2px rgba(0,0,0,.13));
+    }
+
+    .shadow-popup {
+        -webkit-box-shadow: 0px 9px 15px 0px rgba(0,0,0,0.15);
+        -moz-box-shadow: 0px 9px 15px 0px rgba(0,0,0,0.15);
+        box-shadow: 0px 9px 15px 0px rgba(0,0,0,0.15);
+    }
+
+</style>
